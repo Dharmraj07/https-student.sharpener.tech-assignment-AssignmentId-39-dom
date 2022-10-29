62d24eae69f5b7adec04161956b915098f29c5f0
Dharm Raj Hembram <dharmrajh03@gmail.com>
2022-10-29 21:13:48 +0530

    task-10-dom

--- .vscode/js.js
+++
@@ -1,8 +0,0 @@
-var items=document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[0]);
-items[0].textContent='Hello World!!';
-items[2].style.backgroundColor="green"
-for(let i=0; i<items.length; i++){
-    items[i].style.fontWeight='bold';
-}
